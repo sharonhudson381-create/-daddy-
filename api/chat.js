58324ef2646dbc34a27a2e11f25a49876ec17550
sharonhudson381-create <sharonhudson381@gmail.com>
2026-08-09 18:34:09 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-const PERSONA = require('./persona');
+const PERSONA = require('../persona');
 module.exports = async (req, res) => {
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Method not allowed' });
